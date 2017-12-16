c58ebe7695697b8ef9a138f38313481afe25bf21
Vince-V <vincevillas2@gmail.com>
2017-12-15 23:08:24 -0600

    testCommit update.js

--- job_board_serverless/features/update.js
+++ job_board_serverless/features/update.js
@@ -1,1 +1,15 @@
-update.js
+#update.js
+
+const uuid = require("uuid");
+const AWS = require("aws-sdk");
+
+const dynamoDB = new AWS.DynamoDB.DcoumentClient();
+
+dynamoDB.put(params, error => {
+if (error) {
+	console.error('Invalid format sir/mam!: ${JSON.stringify(error)}')
+ } else {
+    return Promise
+ }
+ })
+};
