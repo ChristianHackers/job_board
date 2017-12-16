c7d1c24cd743a082007e5a50c6180d1fe72fc4f8
Vince-V <vincevillas2@gmail.com>
2017-12-16 11:52:18 -0600

    updated read.js file

--- job_board_serverless/features/read.js
+++ job_board_serverless/features/read.js
@@ -1,13 +1,34 @@
 'use strict';
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
 
-module.exports.getJobPost = (event, context, callback) => {
-	const message = 'Thanks for hitting the job board get route. Have a nice day :)'
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: message
-    }),
+const params = {
+    TableName: 'job-posts'
   };
 
-  callback(null, response);
-};
+  module.exports.getJobPost = (event, context, callback) => {
+    dynamoDB.scan(params, (error, result) => {
+      if (error) {
+        console.error(error);
+        callback(new Error('Couldn\'t fetch account'));
+        return;
+      }
+  
+      const response = {
+        statusCode: 200, 
+        body: JSON.stringify(result.Items)
+      }
+      callback(null, response)
+    });
+  }
+
+// module.exports.getJobPost = (event, context, callback) => {
+// 	const message = 'Thanks for hitting the job board get route. Have a nice day :)'
+//   const response = {
+//     statusCode: 200,
+//     body: JSON.stringify({
+//       message: message
+//     }),
+//   };
+
+//   callback(null, response);
+// };
