7e3068a912ec4302e0c9013eb6e2478c237dd8d9
jeremy perez <glyde06@hotmail.com>
2017-12-15 20:45:31 -0600

    Store message in a const variable

--- job_board_serverless/features/read.js
+++ job_board_serverless/features/read.js
@@ -1,10 +1,11 @@
 'use strict';
 
 module.exports.read = (event, context, callback) => {
+	const message = 'Thanks for hitting the job board get route. Have a nice day :)'
   const response = {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!'
+      message: message
     }),
   };
 
