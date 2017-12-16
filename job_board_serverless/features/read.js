7631375e5f054c0e44055bb20e586d7932807a42
jeremy perez <glyde06@hotmail.com>
2017-12-16 12:23:37 -0600

    Add ability to fetch individual jobPost

--- job_board_serverless/features/read.js
+++ job_board_serverless/features/read.js
@@ -21,4 +21,51 @@ module.exports.getJobPosts = (event, context, callback) => {
     }
     callback(null, response)
   });
-}
+};
+
+// module.exports.getJobPost = (event, context, callback) => {
+// 	const todo_id = event.pathParameters.id;
+//   dynamoDB.scan(params, (error, result) => {
+//     if (error) {
+//       console.error(error);
+//       callback(new Error('Couldn\'t fetch account'));
+//       return;
+//     }
+
+//     const response = {
+//       statusCode: 200, 
+//       body: JSON.stringify(result.Items)
+//     }
+//     callback(null, response)
+//   });
+// };
+
+module.exports.getJobPost = (event, context, callback) => {
+  const params = {
+    TableName: process.env.DYNAMODB_TABLE,
+    Key: {
+      id: event.pathParameters.id,
+    },
+  };
+
+  // fetch todo from the database
+  dynamoDB.get(params, (error, result) => {
+    // handle potential errors
+    if (error) {
+      console.error(error);
+      callback(null, {
+        statusCode: error.statusCode || 501,
+        headers: { 'Content-Type': 'text/plain' },
+        body: 'Couldn\'t fetch the todo item.',
+      });
+      return;
+    }
+
+    // create a response
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(result.Item),
+    };
+    callback(null, response);
+  });
+};
