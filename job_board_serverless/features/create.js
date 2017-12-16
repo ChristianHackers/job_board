84ab47b02784cdfeb3810d19398f4d49d64b77b8
jeremy perez <glyde06@hotmail.com>
2017-12-15 23:37:20 -0600

    Modify the create.js file

--- job_board_serverless/features/create.js
+++ job_board_serverless/features/create.js
@@ -1,1 +1,31 @@
-create.js
+const uuid = require("uuid");
+const AWS = require("aws-sdk");
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+module.exports = (jobPostData, businessName) => {
+	console.log("In the save to DB function", jobPostData);
+  const date = JSON.stringify(new Date());
+  const params = {
+    TableName: job-posts,
+    Item: {
+      id: uuid.v1(),
+      businessName: 'Christian Hackers',
+      title: jobPostData.title,
+      shortDescription: jobPostData.shortDescription,
+      longDescription: jobPostData.longDescription,
+      addedAt: date
+    }
+  };
+
+  dynamoDb.put(params, error => {
+    if (error) {
+      console.error(`Error saving data to DynamoDB: ${JSON.stringify(error)}`);
+      return Promise.reject(
+        `Error saving data to DynamoDB: ${JSON.stringify(error)}`
+      );
+    } else {
+      return Promise.resolve(params.Item);
+    }
+  });  
+};
--- job_board_serverless/features/update.js
+++ job_board_serverless/features/update.js
@@ -1,16 +1,1 @@
-#Christian Hackers
-#update.js
-
-const uuid = require("uuid");
-const AWS = require("aws-sdk");
-
-const dynamoDB = new AWS.DynamoDB.DcoumentClient();
-
-dynamoDB.put(params, error => {
-if (error) {
-	console.error('Invalid format sir/mam!: ${JSON.stringify(error)}')
- } else {
-    return Promise
- }
- })
-};
+update.js
