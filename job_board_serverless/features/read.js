1aa2ec101ff21165026afabc950fc96707062b72
jeremy perez <glyde06@hotmail.com>
2017-12-15 20:40:12 -0600

    Paste old handler.js into read.js file with some changes
    Each one of the routes we create is going to execute a function inside a
    different file that we store in the features folder. So if we hit a GET
    route that needs to read data, the GET route will execute the 'read'
    function in the read.js file.

--- job_board_serverless/features/read.js
+++ job_board_serverless/features/read.js
@@ -1,1 +1,12 @@
-read.js
+'use strict';
+
+module.exports.read = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Go Serverless v1.0! Your function executed successfully!'
+    }),
+  };
+
+  callback(null, response);
+};
